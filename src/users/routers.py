8c55b2c8cbea689632689dfbae29e314f98e029d
ProgKenhy <sashok20053@gmail.com>
2025-09-05 09:01:11 +0200

    Update routers.py

--- src/users/routers.py
+++ src/users/routers.py
@@ -1,38 +1,42 @@
-from aiogram import Router
-from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from users.models import User
-
-users_router = Router()
-@users_router.message(CommandStart())
-async def cmd_start(message: Message, session: AsyncSession):
-    await message.answer(text="Hello!")
-
-
-# from aiogram import Router, F
-# from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
-# from aiogram.filters import Command
+# from aiogram import Router
+# from aiogram.filters import Command, CommandStart
+# from aiogram.types import Message
 # from sqlalchemy.ext.asyncio import AsyncSession
 #
-# from app.db.models import User
+# from .models import User
 #
-# router = Router()
-#
-# @router.message(Command("start"))
+# users_router = Router()
+# @users_router.message(CommandStart())
 # async def cmd_start(message: Message, session: AsyncSession):
-#     # Проверяем, есть ли пользователь в БД
-#     user = await session.get(User, message.from_user.id)
-#     if not user:
-#         # Создаем нового пользователя
-#         new_user = User(
-#             telegram_id=message.from_user.id,
-#             username=message.from_user.username,
-#             first_name=message.from_user.first_name
-#         )
-#         session.add(new_user)
-#         await session.commit()
+#     await message.answer(text="Hello!")
+
+
+from aiogram import Router, F
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.filters import Command
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from .models import User
+
+users_router = Router()
+
+@users_router.message(Command("start"))
+async def cmd_start(message: Message, session: AsyncSession):
+    # Проверяем, есть ли пользователь в БД
+    stmt = select(User).where(User.tg_id == message.from_user.id)
+    result = await session.execute(stmt)
+    user = result.scalar_one_or_none()
+    if not user:
+        # Создаем нового пользователя
+        new_user = User(
+            tg_id=message.from_user.id,
+            username=message.from_user.username,
+            first_name=message.from_user.first_name
+        )
+        session.add(new_user)
+        await session.commit()
+        
+
 #         # Просим локацию
 #         location_keyboard = ReplyKeyboardMarkup(
 #             keyboard=[[KeyboardButton(text="Поделиться местоположением", request_location=True)]],
